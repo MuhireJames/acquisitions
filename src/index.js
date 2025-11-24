cb5fef7ccfea44c67a7fd760635da02e4acd83d6
MuhireJames <mujames008@gmail.com>
2025-11-24 15:58:10 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the application!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+export default app;
+
+
+
+
+
+
